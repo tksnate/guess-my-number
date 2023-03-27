e16cc812796dd6b88d3b5f43fa4a19d9c95966de
tksnate <tksnate@gmail.com>
2023-03-26 18:27:19 -0700

    added bootstrap  updated index

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var score = 0;
-var highScore = 20;
-var secretNumber = Number(Math.floor(Math.random() * 20 + 1));
+let score = 0;
+let highScore = 20;
+let secretNumber = Number(Math.floor(Math.random() * 20 + 1));
 
 console.log(secretNumber, typeof secretNumber);
 
@@ -12,33 +12,42 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '❌ No Number!';
-  }
-
-  if (guess != secretNumber) {
+    // } else if (guess != secretNumber) {
+    //   score++;
+    //   document.querySelector('.message').textContent = 'Try again!';
+    //   document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
     score++;
-    document.querySelector('.message').textContent = 'Try again!';
     document.querySelector('.score').textContent = score;
-  }
-
-  if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < secretNumber) {
+    score++;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Too low!';
+  } else if (guess === secretNumber) {
     document.querySelector('.message').textContent =
       'Correct Number!  You win!';
     document.querySelector('.number').textContent = secretNumber;
-
+    document.body.style.background = 'green';
+    // document.querySelector('.number').style = 'width: 20rem;';
+    document.querySelector('.number').style.width = '30rem';
     if (score < highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-    document.body.style.background = 'green';
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 0;
   document.querySelector('.score').textContent = 0;
-  document.body.style.background = 'darkgrey';
+  document.body.style.background = 'black';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = 0;
   secretNumber = Number(Math.floor(Math.random() * 20 + 1));
+  // document.querySelector('.number').style = 'width: 15rem;';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent =
+    'Enter your guess and press "Check!"';
   console.log(secretNumber);
 });
